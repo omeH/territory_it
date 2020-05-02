3acff73aec515991cf74016d92e15422a7463716
Andrey Pakhomau <apahomov@beltechsoftware.com>
2020-05-02 18:42:42 +0300

    Use VALUES command to improve query speed.

--- app/models/authors/finder.rb
+++ app/models/authors/finder.rb
@@ -8,7 +8,10 @@ module Authors
     end
 
     def find
-      relation = UserIp.by_logins(logins)
+      login_binds = ['(?)'] * logins.size
+      condition = "users.login IN (VALUES #{login_binds.join(', ')})"
+
+      relation = UserIp.joins(:user).where(condition, *logins)
 
       aggregate_clause = Arel.sql('ARRAY_AGG(DISTINCT users.login) AS logins')
       relation.select(:ip, aggregate_clause).group(:ip)
